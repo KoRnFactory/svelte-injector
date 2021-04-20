92c7549e9719cc32c415cb787332e92feacc2a2a
Ray Thurne Void <ray.thurne.void@gmail.com>
2021-04-20 05:14:20 +0200

    Prevent the component props to be resetted when unchanged and the {#each} statement runs in InjectedComponents.svelte due to a foreign component being inserted/updated/removed.

--- src/internal/InjectedComponent.svelte
+++ src/internal/InjectedComponent.svelte
@@ -1,3 +1,7 @@
+<!-- Prevent the component props being resetted when another component as been linked into InjectedComponents -->
+<!-- component.props should be considered immutable and to update the component props user has to update the instance -->
+<svelte:options immutable={true} />
+
 <script>
     export let component;
 </script>
